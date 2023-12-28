e2742100a9932b770be2ed4181d6a5d19b9265f3
Tin Nguyen <tinn7624@gmail.com>
2023-12-28 18:23:05 +0700

    up 28 december

---
+++ EnvironmentalProtectionSurvey/Controllers/NewsFEController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EnvironmentalProtectionSurvey.Models;
+
+namespace EnvironmentalProtectionSurvey.Controllers
+{
+    public class NewsFEController : Controller
+    {
+        private readonly Project2Context _context;
+
+        public NewsFEController(Project2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: NewsFE
+        public async Task<IActionResult> Index()
+        {
+              return _context.News != null ? 
+                          View(await _context.News.ToListAsync()) :
+                          Problem("Entity set 'SurveyProjectContext.News'  is null.");
+        }
+
+        // GET: NewsFE/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.News == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+
+        // GET: NewsFE/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NewsFE/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Content,Image")] News news, IFormFile file)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string path = Path.Combine("wwwroot/Images", file.FileName);
+                    var stream = new FileStream(path, FileMode.Create);
+                    file.CopyToAsync(stream);
+
+                    news.Image = "/Images/" + file.FileName;
+                    _context.Add(news);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Fail");
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(string.Empty, e.Message);
+                throw;
+            }
+            return View(news);
+        }
+
+        // GET: NewsFE/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.News == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News.FindAsync(id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+            return View(news);
+        }
+
+        // POST: NewsFE/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,Image")] News news)
+        //{
+        //    if (id != news.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(news);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!NewsExists(news.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(news);
+        //}
+
+        //News edit
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,Image")] News news, IFormFile file)
+        //{
+        //    if (id != news.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            if (file != null && file.Length > 0)
+        //            {
+        //                // Handle image upload
+        //                string path = Path.Combine("wwwroot/Images", file.FileName);
+        //                using (var stream = new FileStream(path, FileMode.Create))
+        //                {
+        //                    await file.CopyToAsync(stream);
+        //                }
+
+        //                news.Image = "/Images/" + file.FileName;
+        //            }
+
+        //            _context.Update(news);
+        //            await _context.SaveChangesAsync();
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!NewsExists(news.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            ModelState.AddModelError(string.Empty, "Error saving news: " + e.Message);
+        //        }
+        //    }
+
+        //    return View(news);
+        //}
+
+
+        // GET: NewsFE/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.News == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+
+        // POST: NewsFE/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.News == null)
+            {
+                return Problem("Entity set 'SurveyProjectContext.News'  is null.");
+            }
+            var news = await _context.News.FindAsync(id);
+            if (news != null)
+            {
+                _context.News.Remove(news);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NewsExists(int id)
+        {
+          return (_context.News?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
